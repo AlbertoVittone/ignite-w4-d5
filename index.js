9260c2d548bb61ad4f061d992047e44e211be16d
diego <diego@getdayone.com>
2020-05-01 20:33:53 +0200

    final

--- index.js
+++ index.js
@@ -20,13 +20,63 @@ async function loadArtist(artistId) {
 	"headers": {
 		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
 		"x-rapidapi-key": "575de39080mshf1f9cab8127c63fp1bcad8jsn113d9f3f814b"
-    }
-})
+    } })
+    
     let artistInfo = await response.json();
     console.log(artistInfo)
 
     document.querySelector("#artist").style.background = `url(${artistInfo.picture_xl})`
     document.querySelector("#artist-name").innerText= artistInfo.name
+
+    let songs = await loadSongs(artistInfo.name)
+    let songsContainer = document.querySelector("#songs")
+
+    //we create a container that will contain the song cards
+    let artistContainer = document.createElement("div")
+    artistContainer.className = "artist-container"
+    //we add a title with "songs for " artist name
+    artistContainer.innerHTML = `<h3 class="container-title">
+        <a href="details.html?id=${songs[0].artist.id}">Songs for ${artistInfo.name} </a>
+    </h3>`
+    //we create a div that will contain all our song cards
+    let songList = document.createElement("div")
+    songList.className = "artist-list";
+    //transform the array of songs into HTML 
+    songs.forEach(song => {
+        let songCard = document.createElement("div")
+        songCard.className ="song-card"
+        songCard.innerHTML =  `<img src="${song.album.cover_big}" alt="David Bowie" class="song-card-image">
+        <div class="song-card-info">
+            <div class="song-card-artist">${song.artist.name}</div>
+            <div class="song-card-album">${song.album.title}</div>
+            <div class="song-card-title">${song.title}</div>
+        </div>
+        <div class="song-card-play"></div>
+        `
+        //when the user clicks on a song
+        songCard.addEventListener("click", (e) => {
+            //take a reference to the player panel
+            let player = document.querySelector("#player")
+            //change the player interface
+            player.innerHTML = `
+            <div class="player-container">
+                <img class="player-image" src="${song.album.cover}" />
+                <p>${song.title} <p/>
+            </div>
+            `
+
+            //take a reference to the audio tag
+            let audioPlayer = document.querySelector("#audio-player")
+            //set the src with the given song preview
+            audioPlayer.src = song.preview
+            //play that song
+            //audioPlayer.play()
+        })
+        songList.appendChild(songCard)
+    })
+    //add it to the main div
+    artistContainer.appendChild(songList)
+    songsContainer.appendChild(artistContainer)
 }
 
 //starting artists
