35d94e181029350a35f6e6d53fad2d9ce2d7dee1
diego <diego@getdayone.com>
2020-05-01 19:16:30 +0200

    comments

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 async function loadSongs(artist) {
+    //get the songs for the given artist
     let response = await fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artist, {
         "method": "GET",
         "headers": {
@@ -7,26 +8,36 @@ async function loadSongs(artist) {
         }
     })
 
+    //transforms them into a javascript object
     let songs = await response.json();
+    //returns just the songs inside of the data array
     return songs.data;
 }
 
-let artists = [ "eminem", "metallica", "madonna", "muse"]
+//starting artists
+let artists = ["eminem", "metallica", "madonna", "muse"]
 
 async function loadArtists() {
 
+    //getting a reference to songs div and remove everything from inside
     let songsContainer = document.querySelector("#songs")
     songsContainer.innerHTML = ""
 
+    //for each artist in our array
     artists.forEach(async artist => {
+        //we fetch 25 songs
         let songs = await loadSongs(artist)
+        //we create a container that will contain the song cards
         let artistContainer = document.createElement("div")
-        artistContainer.className="artist-container"
+        artistContainer.className = "artist-container"
+        //we add a title with "songs for " artist name
         artistContainer.innerHTML = `<h3 class="container-title">Songs for ${artist}</h3>`
+        //we create a div that will contain all our song cards
         let songList = document.createElement("div")
-        songList.className ="artist-list";
-        songList.innerHTML = songs.map(song => 
-            ` <div class="song-card">
+        songList.className = "artist-list";
+        //transform the array of songs into HTML 
+        songList.innerHTML = songs.map(song =>
+          `<div class="song-card">
             <img src="${song.album.cover_big}" alt="David Bowie" class="song-card-image">
             <div class="song-card-info">
               <div class="song-card-artist">${song.artist.name}</div>
@@ -35,8 +46,8 @@ async function loadArtists() {
             </div>
             <div class="song-card-play"></div>
           </div>`).join("")
-          artistContainer.appendChild(songList)
-          songsContainer.appendChild(artistContainer)
+        //add it to the main div
+        artistContainer.appendChild(songList)
+        songsContainer.appendChild(artistContainer)
     })
-
 }
